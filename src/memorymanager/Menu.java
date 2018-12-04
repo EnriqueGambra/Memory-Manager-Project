63e4b198a7bbf4bb23c018fa206faea84eed4e7c
Enrique Gambra <gambra7@gmail.com>
2018-12-04 03:16:24 -0500

    Fixed Issues within Code

--- src/memorymanager/Menu.java
+++ src/memorymanager/Menu.java
@@ -12,22 +12,13 @@ public class Menu {
     private int pid, processSize;
     private int min = 0;
     private int maxCapacity = 0;
-<<<<<<< HEAD
-    private int maxForAll = 500;
-    private boolean processAdded = true;
-    private ArrayList<MemoryBlock> blocks = new ArrayList();
-    private ArrayList<MemoryBlock> freeMemory = new ArrayList();
-    private ArrayList<MemoryBlock> waitingQueue = new ArrayList();
-    private ArrayList<MemoryBlock> removedProcesses = new ArrayList();
-    private boolean maxNotReached = false;
-=======
     static private int maxForAll = 500;
     private boolean processAdded = true;
     static private ArrayList<MemoryBlock> blocks = new ArrayList();
     private ArrayList<MemoryBlock> freeMemory = new ArrayList();
     private ArrayList<MemoryBlock> removedProcesses = new ArrayList();
     private boolean maxReached = false;
->>>>>>> master
+
     private boolean notFitInRemoved = false;
     
     public Menu(){
@@ -65,61 +56,8 @@ public class Menu {
             
             switch(menuChoice){
                 case 1:
-                    input();
-<<<<<<< HEAD
-                    
-                        if(blocks.size() == 0){
-                            System.out.println("Hi");
-                            //maxNotReached = checkOverflow();
-                            //if(maxNotReached == true)
-                            //{
-                            blocks.add(new MemoryBlock(this.pid,this.processSize, 0, this.processSize));
-                            //}
-                        }else
-                        {
-                            //maxNotReached = checkOverflow();
-                            //if(maxNotReached == true)
-                            //{
-                            if(removedProcesses.size() > 0)
-                            {
-                                for(int i = 0; i < removedProcesses.size(); i++)
-                                {
-                                    if(removedProcesses.get(i).getProcessSize() >= processSize)
-                                    {
-                                        MemoryBlock newBlock = new MemoryBlock(this.pid, this.processSize, removedProcesses.get(i).getMin(), 
-                                                                removedProcesses.get(i).getMin() + processSize);
-                                        notFitInRemoved = true;
-                                        System.out.println("In here!");
-                                        System.out.println("PID = " + newBlock.getPid() + "Max = " + newBlock.getMax() + "Min = " + newBlock.getMin());
-                                        blocks.trimToSize();
-                                        blocks.add(newBlock);
-                                        break;
-                                    }
-                                    break;
-                                }
-                                if(notFitInRemoved == true)
-                                {
-                                    continue;
-                                }
-                                else
-                                {
-                                    MemoryBlock newBlock = new MemoryBlock(this.pid,this.processSize, blocks.get(blocks.size() - 1).getMax(), 0);
-                                    newBlock.setMax(newBlock.getMin() + newBlock.getProcessSize());
-                                    blocks.add(newBlock);
-                                }
-                            }
-                            else
-                            {
-                            MemoryBlock newBlock = new MemoryBlock(this.pid,this.processSize, blocks.get(blocks.size() - 1).getMax(), 0);
-                            newBlock.setMax(newBlock.getMin() + newBlock.getProcessSize());
-                            blocks.add(newBlock);
-                            }
-                        }
-                        System.out.println(blocks.get(blocks.size() - 1).toString());
-=======
-                   
+                    input();         
                         //System.out.println(blocks.get(blocks.size() - 1).toString());
->>>>>>> master
                     break;
                 case 2:
                     break;
@@ -128,13 +66,10 @@ public class Menu {
                     process = input.nextInt();
                     for(int i = 0; i < blocks.size(); i++) {
                         if(process == blocks.get(i).getPid()){
-<<<<<<< HEAD
-                            removeProcess(i);
-=======
                             blocks.remove(i);
                             pids.remove(i);
                             System.out.println("removed successfully");
->>>>>>> master
+
                         }
                     }
                     
@@ -143,26 +78,18 @@ public class Menu {
                     break;
                 case 5:
                     System.out.println();
-<<<<<<< HEAD
-                    System.out.println("PID         Process Size        Min          Max");
-                    for(int i = 0; i < blocks.size(); i++){
-                        System.out.printf("%d           %d                   %d            %d\n", 
-=======
+
                     System.out.println("PID\t\tProcess Size\t\tMin\t\tMax");
                     for(int i = 0; i < blocks.size(); i++){
                         System.out.printf("%d\t\t%d\t\t\t%d\t\t%d\n", 
->>>>>>> master
+
                                 blocks.get(i).getPid(),
                                 blocks.get(i).getProcessSize(),
                                 blocks.get(i).getMin(),
                                 blocks.get(i).getMax());  
-<<<<<<< HEAD
-                        //System.out.println("Blocks size = " + blocks.size());
+
                     }
-                    
-=======
-                    }
->>>>>>> master
+
                     break;
                 case 6:
                     break;
@@ -171,8 +98,7 @@ public class Menu {
                 default:
                     System.out.println("Invalid input");
                     break;          
-<<<<<<< HEAD
-=======
+
             }
         }
     }
@@ -240,7 +166,7 @@ public class Menu {
                 MemoryBlock newBlock = new MemoryBlock(this.pid,this.processSize, blocks.get(blocks.size() - 1).getMax(), 0);
                 newBlock.setMax(newBlock.getMin() + newBlock.getProcessSize());
                 blocks.add(newBlock);
->>>>>>> master
+
             }
             
             
@@ -251,59 +177,7 @@ public class Menu {
 //            }
         }
     }
-    
-<<<<<<< HEAD
-    
-    public void input(){
-        System.out.print("Enter process id: ");
-        pid = input.nextInt();
-        System.out.print("Enter process size: ");
-        processSize = input.nextInt();
-        
-        
-        if(pid < 0){
-            System.out.println("You CANNOT enter a pid number less than 1");
-            input();
-        }
-        
-        if(processSize < 1){
-            System.out.println("You CANNOT enter a process size less than 1");
-            input();
-        }else if(processSize > 500){
-            System.out.println("You CANNOT enter a process size greater than 500");
-            input();
-        }
-        
-        //checks for pids, if they are in array already
-        for(int i = 0; i < pids.size(); i++){
-            if(pid == pids.get(i)){
-                System.out.println("PID is in memory already");
-                inArray = true;
-                
-            }
-        }
-        if(!inArray){
-            pids.add(pid);
-            System.out.println("PID added successfully");
-        }
-    }
-    
-    public boolean checkOverflow()
-    {
-        maxForAll = 500;
-        for(int i = 0; blocks.size() > i; i++)
-        {
-            maxForAll = maxForAll - blocks.get(i).getProcessSize();
-        }
-        
-        if(maxCapacity < maxForAll)
-        {
-            System.out.println("Process " + pid + " does not fit within the memory. It is being added to the waiting queue." );
-            waitingQueue.add(new MemoryBlock(this.pid,this.processSize, 0, 0));
-            return false;
-        }
-        return true;
-=======
+  
     public static boolean checkOverflow(int processSize){
         maxForAll = 500;
         for(int i = 0; i < blocks.size(); i++){
@@ -318,7 +192,7 @@ public class Menu {
         }else{
             return false;
         }
->>>>>>> master
+
     }
     
     public void removeProcess(int i)
@@ -327,8 +201,5 @@ public class Menu {
         System.out.println("Process " + blocks.get(i).getPid() + " was removed sucessfully!");
         blocks.remove(i);
     }
-<<<<<<< HEAD
-=======
-    
->>>>>>> master
+
 }
